da26b14b9d5d4b1e49e892bba28696a94d77a973
KC <KC@idea-PC>
2018-04-16 08:59:28 +0530

    renamed variables and methods in junits

--- src/test/java/com/ubs/opsit/interviews/berlinclock/HoursIndicatorTest.java
+++ src/test/java/com/ubs/opsit/interviews/berlinclock/HoursIndicatorTest.java
@@ -11,154 +11,154 @@ public class HoursIndicatorTest {
 	public void testBerlinHour1() {
 		
 		HoursIndicator hours = new HoursIndicator(1);
-		assertFirstAndSecondRow(hours, "OOOO", "ROOO");	
+		assertSecondAndThirdRow(hours, "OOOO", "ROOO");	
 	}
 	
 	@Test
 	public void testBerlinHour2() {
 		
 		HoursIndicator hours = new HoursIndicator(2);
-		assertFirstAndSecondRow(hours, "OOOO", "RROO");	
+		assertSecondAndThirdRow(hours, "OOOO", "RROO");	
 	}
 	
 	@Test
 	public void testBerlinHour3() {
 		
 		HoursIndicator hours = new HoursIndicator(3);
-		assertFirstAndSecondRow(hours, "OOOO", "RRRO");	
+		assertSecondAndThirdRow(hours, "OOOO", "RRRO");	
 	}
 	
 	@Test
 	public void testBerlinHour4() {
 		
 		HoursIndicator hours = new HoursIndicator(4);
-		assertFirstAndSecondRow(hours, "OOOO", "RRRR");	
+		assertSecondAndThirdRow(hours, "OOOO", "RRRR");	
 	}
 	
 	@Test
 	public void testBerlinHour5() {
 		
 		HoursIndicator hours = new HoursIndicator(5);
-		assertFirstAndSecondRow(hours, "ROOO", "OOOO");
+		assertSecondAndThirdRow(hours, "ROOO", "OOOO");
 	}
 	
 	@Test
 	public void testBerlinHour6() {
 		
-		assertFirstAndSecondRow(new HoursIndicator(6), "ROOO", "ROOO");
+		assertSecondAndThirdRow(new HoursIndicator(6), "ROOO", "ROOO");
 	}
 	
 	@Test
 	public void testBerlinHour7() {
 		
-		assertFirstAndSecondRow(new HoursIndicator(7), "ROOO", "RROO");
+		assertSecondAndThirdRow(new HoursIndicator(7), "ROOO", "RROO");
 	}
 	
 	@Test
 	public void testBerlinHour8() {
 		
-		assertFirstAndSecondRow(new HoursIndicator(8), "ROOO", "RRRO");
+		assertSecondAndThirdRow(new HoursIndicator(8), "ROOO", "RRRO");
 	}
 
 	@Test
 	public void testBerlinHour9() {
 		
-		assertFirstAndSecondRow(new HoursIndicator(9), "ROOO", "RRRR");
+		assertSecondAndThirdRow(new HoursIndicator(9), "ROOO", "RRRR");
 	}
 
 	@Test
 	public void testBerlinHour10() {
 		
-		assertFirstAndSecondRow(new HoursIndicator(10), "RROO", "OOOO");
+		assertSecondAndThirdRow(new HoursIndicator(10), "RROO", "OOOO");
 	}
 	
 	@Test
 	public void testBerlinHour11() {
 		
-		assertFirstAndSecondRow(new HoursIndicator(11), "RROO", "ROOO");
+		assertSecondAndThirdRow(new HoursIndicator(11), "RROO", "ROOO");
 	}
 	
 	@Test
 	public void testBerlinHour12() {
 		
-		assertFirstAndSecondRow(new HoursIndicator(12), "RROO", "RROO");
+		assertSecondAndThirdRow(new HoursIndicator(12), "RROO", "RROO");
 	}
 	
 	@Test
 	public void testBerlinHour13() {
 		
-		assertFirstAndSecondRow(new HoursIndicator(13), "RROO", "RRRO");
+		assertSecondAndThirdRow(new HoursIndicator(13), "RROO", "RRRO");
 	}
 	
 	@Test
 	public void testBerlinHour14() {
 		
-		assertFirstAndSecondRow(new HoursIndicator(14), "RROO", "RRRR");
+		assertSecondAndThirdRow(new HoursIndicator(14), "RROO", "RRRR");
 	}
 	
 	@Test
 	public void testBerlinHour15() {
 		
-		assertFirstAndSecondRow(new HoursIndicator(15), "RRRO", "OOOO");
+		assertSecondAndThirdRow(new HoursIndicator(15), "RRRO", "OOOO");
 	}
 	
 	@Test
 	public void testBerlinHour16() {
 		
-		assertFirstAndSecondRow(new HoursIndicator(16), "RRRO", "ROOO");
+		assertSecondAndThirdRow(new HoursIndicator(16), "RRRO", "ROOO");
 	}
 	
 	@Test
 	public void testBerlinHour17() {
 		
-		assertFirstAndSecondRow(new HoursIndicator(17), "RRRO", "RROO");
+		assertSecondAndThirdRow(new HoursIndicator(17), "RRRO", "RROO");
 	}
 	
 	@Test
 	public void testBerlinHour18() {
 		
-		assertFirstAndSecondRow(new HoursIndicator(18), "RRRO", "RRRO");
+		assertSecondAndThirdRow(new HoursIndicator(18), "RRRO", "RRRO");
 	}
 	
 	@Test
 	public void testBerlinHour19() {
 		
-		assertFirstAndSecondRow(new HoursIndicator(19), "RRRO", "RRRR");
+		assertSecondAndThirdRow(new HoursIndicator(19), "RRRO", "RRRR");
 	}
 	
 	@Test
 	public void testBerlinHour20() {
 		
-		assertFirstAndSecondRow(new HoursIndicator(20), "RRRR", "OOOO");
+		assertSecondAndThirdRow(new HoursIndicator(20), "RRRR", "OOOO");
 	}
 	
 	@Test
 	public void testBerlinHour21() {
 		
-		assertFirstAndSecondRow(new HoursIndicator(21), "RRRR", "ROOO");
+		assertSecondAndThirdRow(new HoursIndicator(21), "RRRR", "ROOO");
 	}
 	
 	@Test
 	public void testBerlinHour22() {
 		
-		assertFirstAndSecondRow(new HoursIndicator(22), "RRRR", "RROO");
+		assertSecondAndThirdRow(new HoursIndicator(22), "RRRR", "RROO");
 	}
 	
 	@Test
 	public void testBerlinHour23() {
 		
-		assertFirstAndSecondRow(new HoursIndicator(23), "RRRR", "RRRO");
+		assertSecondAndThirdRow(new HoursIndicator(23), "RRRR", "RRRO");
 	}
 	
 	@Test
 	public void testBerlinHour24() {
 		
-		assertFirstAndSecondRow(new HoursIndicator(24), "RRRR", "RRRR");
+		assertSecondAndThirdRow(new HoursIndicator(24), "RRRR", "RRRR");
 	}
 		
-	private void assertFirstAndSecondRow(HoursIndicator hours, String expectedFirstRow, String expectedSecondRow) {
+	private void assertSecondAndThirdRow(HoursIndicator hours, String expectedSecondRow, String expectedThirdRow) {
 		
-		Assert.assertEquals("First Row Did not match", expectedFirstRow, String.valueOf(hours.secondRowLamps));
-		Assert.assertEquals("Second Row Did not match", expectedSecondRow, String.valueOf(hours.thirdRowLamps));
+		Assert.assertEquals("First Row Did not match", expectedSecondRow, String.valueOf(hours.secondRowLamps));
+		Assert.assertEquals("Second Row Did not match", expectedThirdRow, String.valueOf(hours.thirdRowLamps));
 	}
 }
--- src/test/java/com/ubs/opsit/interviews/berlinclock/MinutesIndicatorTest.java
+++ src/test/java/com/ubs/opsit/interviews/berlinclock/MinutesIndicatorTest.java
@@ -10,109 +10,109 @@ public class MinutesIndicatorTest {
 	@Test
 	public void testMinutes1() {
 		
-		assertFirstAndSecondRow(new MinutesIndicator(1), "OOOOOOOOOOO", "YOOO");
+		assertFourthAndFifth(new MinutesIndicator(1), "OOOOOOOOOOO", "YOOO");
 	}
 	
 	@Test
 	public void testMinutes2() {
 		
-		assertFirstAndSecondRow(new MinutesIndicator(2), "OOOOOOOOOOO", "YYOO");
+		assertFourthAndFifth(new MinutesIndicator(2), "OOOOOOOOOOO", "YYOO");
 	}
 	
 	@Test
 	public void testMinutes3() {
 		
-		assertFirstAndSecondRow(new MinutesIndicator(3), "OOOOOOOOOOO", "YYYO");
+		assertFourthAndFifth(new MinutesIndicator(3), "OOOOOOOOOOO", "YYYO");
 	}
 
 	@Test
 	public void testMinutes4() {
 		
-		assertFirstAndSecondRow(new MinutesIndicator(4), "OOOOOOOOOOO", "YYYY");
+		assertFourthAndFifth(new MinutesIndicator(4), "OOOOOOOOOOO", "YYYY");
 	}
 	
 	@Test
 	public void testMinutes5() {
 		
-		assertFirstAndSecondRow(new MinutesIndicator(5), "YOOOOOOOOOO", "OOOO");
+		assertFourthAndFifth(new MinutesIndicator(5), "YOOOOOOOOOO", "OOOO");
 	}
 	
 	@Test
 	public void testMinutes6() {
 		
-		assertFirstAndSecondRow(new MinutesIndicator(6), "YOOOOOOOOOO", "YOOO");
+		assertFourthAndFifth(new MinutesIndicator(6), "YOOOOOOOOOO", "YOOO");
 	}
 	
 	@Test
 	public void testMinutes7() {
 		
-		assertFirstAndSecondRow(new MinutesIndicator(7), "YOOOOOOOOOO", "YYOO");
+		assertFourthAndFifth(new MinutesIndicator(7), "YOOOOOOOOOO", "YYOO");
 	}
 	
 	@Test
 	public void testMinutes8() {
 		
-		assertFirstAndSecondRow(new MinutesIndicator(8), "YOOOOOOOOOO", "YYYO");
+		assertFourthAndFifth(new MinutesIndicator(8), "YOOOOOOOOOO", "YYYO");
 	}
 	
 	@Test
 	public void testMinutes9() {
 		
-		assertFirstAndSecondRow(new MinutesIndicator(9), "YOOOOOOOOOO", "YYYY");
+		assertFourthAndFifth(new MinutesIndicator(9), "YOOOOOOOOOO", "YYYY");
 	}
 	
 	@Test
 	public void testMinutes10() {
 		
-		assertFirstAndSecondRow(new MinutesIndicator(10), "YYOOOOOOOOO", "OOOO");
+		assertFourthAndFifth(new MinutesIndicator(10), "YYOOOOOOOOO", "OOOO");
 	}
 	
 	@Test
 	public void testMinutes11() {
 		
-		assertFirstAndSecondRow(new MinutesIndicator(11), "YYOOOOOOOOO", "YOOO");
+		assertFourthAndFifth(new MinutesIndicator(11), "YYOOOOOOOOO", "YOOO");
 	}
 	
 	@Test
 	public void testMinutes12() {
 		
-		assertFirstAndSecondRow(new MinutesIndicator(12), "YYOOOOOOOOO", "YYOO");
+		assertFourthAndFifth(new MinutesIndicator(12), "YYOOOOOOOOO", "YYOO");
 	}
 
 	@Test
 	public void testMinutes13() {
 		
-		assertFirstAndSecondRow(new MinutesIndicator(13), "YYOOOOOOOOO", "YYYO");
+		assertFourthAndFifth(new MinutesIndicator(13), "YYOOOOOOOOO", "YYYO");
 	}
 
 	@Test
 	public void testMinutes14() {
 		
-		assertFirstAndSecondRow(new MinutesIndicator(14), "YYOOOOOOOOO", "YYYY");
+		assertFourthAndFifth(new MinutesIndicator(14), "YYOOOOOOOOO", "YYYY");
 	}
 
 	@Test
 	public void testMinutes15() {
 		
-		assertFirstAndSecondRow(new MinutesIndicator(15), "YYROOOOOOOO", "OOOO");
+		assertFourthAndFifth(new MinutesIndicator(15), "YYROOOOOOOO", "OOOO");
 	}
 	
 
 	@Test
 	public void testMinutes16() {
 		
-		assertFirstAndSecondRow(new MinutesIndicator(16), "YYROOOOOOOO", "YOOO");
+		assertFourthAndFifth(new MinutesIndicator(16), "YYROOOOOOOO", "YOOO");
 	}
 	
 	@Test
 	public void testMinutes59() {
 		
-		assertFirstAndSecondRow(new MinutesIndicator(59), "YYRYYRYYRYY", "YYYY");
+		assertFourthAndFifth(new MinutesIndicator(59), "YYRYYRYYRYY", "YYYY");
 	}
 	
-	private void assertFirstAndSecondRow(MinutesIndicator berlinMinutes, String firstRow, String secondRow) {
+	private void assertFourthAndFifth(MinutesIndicator berlinMinutes, String expectedFourthRow, String expectedFifthRow) {
 		
-		Assert.assertEquals("First Row did not match", firstRow, String.valueOf(berlinMinutes.fourthRowLamps));
-		Assert.assertEquals("Second Row did not match", secondRow, String.valueOf(berlinMinutes.fifthRowLamps));
+		Assert.assertEquals("First Row did not match", expectedFourthRow, String.valueOf(berlinMinutes.fourthRowLamps));
+		Assert.assertEquals("Second Row did not match", expectedFifthRow, String.valueOf(berlinMinutes.fifthRowLamps));
 	}
 }
